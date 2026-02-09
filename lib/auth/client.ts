b85d166d4e458219922bab1e027cb89e154cfb3f
petekipruto <petekipruto@gmail.com>
2026-02-09 10:05:10 +0000

    refactored the  client auth

--- lib/auth/client.ts
+++ lib/auth/client.ts
@@ -1,7 +1,3 @@
 'use client';
 import { createAuthClient } from '@neondatabase/auth/next';
-
-const neonAuthUrl = process.env.NEXT_PUBLIC_NEON_AUTH_BASE_URL || 
-  'https://ep-spring-boat-aisxzatm.neonauth.c-4.us-east-1.aws.neon.tech/neondb/auth';
-
-export const authClient = createAuthClient(neonAuthUrl);
+export const authClient = createAuthClient();
